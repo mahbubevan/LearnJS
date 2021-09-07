603f709c5ba2bb6df0e71a63bc36da8c2ec18da4
mahbubevan <evanpulse92@gmail.com>
2021-09-07 23:22:25 +0600

    Arithmatic and  Data Type

---
+++ basic/arithmatic.js
@@ -0,0 +1,48 @@
+// Add 
+let x = 5
+let y = 6
+let z = x + y
+console.log(z);
+
+// Sub 
+
+let a = 10 
+let b = 4 
+let c = a - b
+console.log(c); 
+
+// Multiplication 
+var p = 5
+var q = 3 
+var r = p*q 
+console.log(r,"Multiplication");
+
+// Divide 
+r = p/q 
+console.log(r,"Divide");
+
+// Remainder 
+p = 8
+r = p % q 
+console.log(r,"Remainder");
+
+// Incrementing 
+r = 2
+r++
+console.log(r,"Incrementing");
+
+// Decrementing
+r--
+console.log(r,"Decrementing");
+
+// Exponentiation 
+ var p = 5
+ var q = 2
+ var r = p ** q 
+
+ console.log(r,"Exponentiation");
+
+ // Operator Precedence 
+
+ var result = 100 + 3 * 6 / 3  
+ console.log(result);
---
+++ basic/dataType.js
@@ -0,0 +1,6 @@
+var _result = 50 +5 + "Hello"
+_result = "Evan " + 4 +1
+console.log(typeof(_result));
+let x
+x = 20
+console.log(typeof(x));
